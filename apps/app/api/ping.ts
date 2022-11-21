6aa80ab8683bfa93cd47f1807635d6866b0de4bd
TaylorFacen <taylor.facen@gmail.com>
2022-11-21 11:44:47 -0500

    Triggering a build

--- apps/app/api/ping.ts
+++ apps/app/api/ping.ts
@@ -2,6 +2,6 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(request: VercelRequest, response: VercelResponse) {
   response.status(200).json({
-    body: { message: 'pong' }
+    body: { message: 'pong' } 
   });
 }
